bfd0fe970e9309d9864fbffd5901d1199512e678
Cat Moca <cat.r.mocha@gmail.com>
2021-06-22 23:52:58 -0500

    progress commit

--- fiat-google-groups/src/test/groovy/com/netflix/spinnaker/fiat/roles/google/GoogleDirectoryUserRolesProviderSpec.groovy
+++ fiat-google-groups/src/test/groovy/com/netflix/spinnaker/fiat/roles/google/GoogleDirectoryUserRolesProviderSpec.groovy
@@ -1,5 +1,7 @@
 package com.netflix.spinnaker.fiat.roles.google
 
+import com.google.api.services.admin.directory.Directory
+import com.google.api.client.googleapis.batch.BatchRequest;
 import com.netflix.spinnaker.fiat.permissions.ExternalUser
 import com.google.api.services.admin.directory.model.Group;
 import com.google.api.services.admin.directory.model.Groups;
@@ -65,6 +67,83 @@ class GoogleDirectoryUserRolesProviderSpec extends Specification {
 
 
     }
+
+    def "should not call Google API to load role for managed service account"() {
+        setup:
+        config.domain = "test.com"
+        def group = new Group()
+        def userEmail = "0000-00-00-00-000000@managed-service-account"
+        group.set("name", "test")
+        group.set("email", userEmail)
+        List<Group> groupList = new ArrayList<>()
+        groupList.add(group)
+        def groups = new Groups()
+        groups.setGroups(groupList)
+
+        GoogleDirectoryUserRolesProvider provider = new GoogleDirectoryUserRolesProvider() {
+            @Override
+            Groups getGroupsFromEmail(String email) {
+                return groups
+            }
+        }
+
+        provider.setProperty("config", config)
+
+        when:
+        def roles = provider.loadRoles(externalUser(userEmail))
+
+        then:
+        roles == new ArrayList()
+
+    }
+
+    def "should not call Google API to load multiple role for managed service accounts"() {
+        setup:
+        List<Group> groupList = new ArrayList<>()
+
+        // TODO figure out where to return the groups
+        config.domain = "test.com"
+        def group1 = new Group()
+        group1.set("name", "test")
+        group1.set("email", "test@test.com")
+        groupList.add(group1)
+
+        def group2 = new Group()
+        def userEmailManaged = "0000-00-00-00-000000@managed-service-account"
+        group1.set("name", "managedUserGroup")
+        group2.set("email", userEmailManaged)
+        groupList.add(group2)
+
+        def groups = new Groups()
+        groups.setGroups(groupList)
+
+        def service = Mock(Directory)
+        // this doesn't seem to be mocked correctly since it tries to call the google api getRequestFactory D:
+        service.batch(_) >> GroovyMock(BatchRequest)
+
+        GoogleDirectoryUserRolesProvider provider = new GoogleDirectoryUserRolesProvider() {
+            @Override
+            Directory getDirectoryService() {
+                return service
+            }
+
+            @Override
+            Groups getGroupsFromEmail(String email) {
+                return groups
+            }
+        }
+
+        provider.setProperty("config", config)
+
+        when:
+        def results = provider.multiLoadRoles([externalUser("test@test.com"), externalUser("0000-00-00-00-000000@managed-service-account")])
+
+        then:
+        //
+        results.size() == 1
+        results.containsKey("test@test.com")
+    }
+
 
     private static ExternalUser externalUser(String id) {
         return new ExternalUser().setId(id)
